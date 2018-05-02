42c46dfa3ee4bac3184e665bc67ef900cf133b8e
Will <willyboy_43@hotmail.com>
2018-05-02 15:30:45 +0100

    Some frontend when no tab is opened

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,11 +1,15 @@
 <template>
     <section>
         <Nav />
-        <TabContent />
+        <TabContent v-if="activeTab" />
+        <div class="no-tabs" v-if="!activeTab">
+            <div class="no-tabs__description">Open a new tab by clicking the <span class="icon-plus">+</span> icon</div>
+        </div>
     </section>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Nav from '@/components/Nav'
 import TabContent from '@/components/TabContent'
 import LogsList from '@/components/logs/LogsList'
@@ -16,7 +20,12 @@ export default {
         TabContent,
         LogsList
     },
-    name: 'App'
+    name: 'App',
+    computed: {
+        ...mapGetters([
+            'activeTab'
+        ])
+    }
 }
 </script>
 
@@ -45,5 +54,30 @@ select, input {
 }
 .row {
     margin-bottom: 1rem;
+}
+.no-tabs {
+    position: absolute;
+    top: 39px;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    text-align: center;
+    background-color: #eee;
+    justify-content: center;
+    align-items: center;
+}
+.no-tabs .no-tabs__description {
+    color: #888;
+}
+.no-tabs .no-tabs__description .icon-plus {
+    display: inline-block;
+    background-color: white;
+    color: #bdbdbd;
+    border: 1px solid #ddd;
+    width: 38px;
+    height: 38px;
+    line-height: 38px;
+    margin: 0 5px;
 }
 </style>
